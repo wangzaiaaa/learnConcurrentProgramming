43e3bfc53e3e1524c67af56818ab07e05d0e380d
bonsoirzw <2627894288@qq.com>
2019-05-15 15:17:19 +0800

    Chapter10

---
+++ src/Chapter10/ApplicationClassLoader.java
@@ -0,0 +1,8 @@
+package Chapter10;
+
+public class ApplicationClassLoader {
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("java.class.path"));
+        System.out.println(ApplicationClassLoader.class.getClassLoader());
+    }
+}
